08278a43e1bf3a73fbf03e899b7fa19c5cd620c5
HaruwenTech <empresatech.ar@gmail.com>
2025-09-13 22:14:05 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -361,7 +361,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const imgElement = whatsappButton.querySelector('img');
                 if (imgElement) {
                     whatsappButton.innerHTML = `
-                    <img src="/img/icons8-whatsapp-48.png" alt="Whatsapp" width="24"> 
+                    <img src="img/icons8-whatsapp-48.png" alt="Whatsapp" width="24"> 
                     ${translation.sendWhatsapp}
                 `;
                 }
@@ -673,4 +673,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     // Llamar a la función cuando se abre el modal
     document.getElementById('galleryModal').addEventListener('shown.bs.modal', initMobileCarousel);
+
 });
